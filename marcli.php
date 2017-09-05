78b440ed2b71db941147c7a081942a2864ca4ce8
bartikowskiw <bartikowskiw@missouri.edu>
2017-09-05 16:47:23 -0500

    Added replace command.

--- marcli.php
+++ marcli.php
@@ -1,6 +1,11 @@
 <?php
 
 declare( strict_types = 1 );
+
+if ( version_compare( phpversion(), '7.0', '<' ) ) {
+    die( 'This script needs at least PHP version 7.0! (Running on PHP ' . phpversion() .' now.)' . "\n" );
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
@@ -8,6 +13,7 @@ use Umlts\Marcli\CountCommand;
 use Umlts\Marcli\DumpCommand;
 use Umlts\Marcli\LintCommand;
 use Umlts\Marcli\FindCommand;
+use Umlts\Marcli\ReplaceCommand;
 use Umlts\Marcli\MapWriteCommand;
 use Umlts\Marcli\MapReadCommand;
 
@@ -17,6 +23,7 @@ $app->add( new DumpCommand() );
 $app->add( new CountCommand() );
 $app->add( new LintCommand() );
 $app->add( new FindCommand() );
+$app->add( new ReplaceCommand() );
 $app->add( new MapWriteCommand() );
 $app->add( new MapReadCommand() );
 
---
+++ src/ReplaceCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+declare( strict_types = 1 );
+namespace Umlts\Marcli;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Umlts\MarcToolset\MarcReplace;
+use Umlts\MarcToolset\MarcMask;
+
+class ReplaceCommand extends Command {
+
+    protected function configure() {
+
+        $this
+            ->setName( 'marc:replace' )
+            ->setDescription( 'Finds and replaces fields in records.' )
+            ->addArgument( 'needle', InputArgument::REQUIRED, 'Needle (PCRE expression)' )
+            ->addArgument( 'replace', InputArgument::REQUIRED, 'Replacement. See <https://secure.php.net/manual/en/function.preg-replace.php>.' )
+            ->addArgument( 'marc-file', InputArgument::OPTIONAL, 'Path to MARC file', 'php://stdin' )
+            ->addOption( 'tag', 't', InputOption::VALUE_REQUIRED, 'Marc Tag', '...' )
+            ->addOption( 'ind1', NULL, InputOption::VALUE_REQUIRED, 'Indicator 1', '.' )
+            ->addOption( 'ind2', NULL, InputOption::VALUE_REQUIRED, 'Indicator 2', '.' )
+            ->addOption( 'sub', 's', InputOption::VALUE_REQUIRED, 'Subfield', '.' )
+            ->addOption( 'raw', 'r', InputOption::VALUE_NONE, 'Raw MARC output?' );
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output ) {
+
+        $mask = new MarcMask(
+                    $input->getOption( 'tag' ),
+                    $input->getOption( 'ind1' ),
+                    $input->getOption( 'ind2' ),
+                    $input->getOption( 'sub' ),
+                    $input->getArgument( 'needle' )
+                );
+
+        $mf = new MarcReplace( $input->getArgument( 'marc-file' ), $mask );
+
+        if ( $input->getOption( 'raw' ) ) {
+            $mf->replaceAndEchoRaw( $input->getArgument( 'replace' ) );
+        } else {
+            $mf->replaceAndEchoDump( $input->getArgument( 'replace' ), !$input->getOption( 'no-ansi' ) );
+        }
+    }
+
+}
