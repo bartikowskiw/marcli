818140c0e3f1e14320516c55022fa459fc277fe0
bartikowskiw <bartikowskiw@missouri.edu>
2017-09-20 13:30:39 -0500

    Db name is optional now.

--- src/MapWriteCommand.php
+++ src/MapWriteCommand.php
@@ -10,20 +10,27 @@ use Symfony\Component\Console\Input\InputArgument;
 use Umlts\MarcToolset\MarcMapWriter;
 
 class MapWriteCommand extends Command {
-    
+
     protected function configure() {
-        
+
         $this
             ->setName( 'map:write' )
             ->setDescription( 'Creates a SQLite DB with the ids and positions of the records.' )
             ->addArgument( 'marc_file', InputArgument::REQUIRED, 'Path to MARC file' )
-            ->addArgument( 'sqlite_file', InputArgument::REQUIRED, 'Path to SQLite file' );
+            ->addArgument( 'sqlite_file', InputArgument::OPTIONAL, 'Path to SQLite file. Defaults to the MARC file name with added ".db" extension.' );
+
     }
-    
+
+    protected function initialize( InputInterface $input, OutputInterface $output ) {
+        if ( empty( $input->getArgument( 'sqlite_file' ) ) ) {
+            $input->setArgument( 'sqlite_file', $input->getArgument( 'marc_file'  ) . '.db' );
+        }
+    }
+
     protected function execute( InputInterface $input, OutputInterface $output ) {
         $db = new \SQLite3( $input->getArgument( 'sqlite_file' ) );
         $mw = new MarcMapWriter( $input->getArgument( 'marc_file' ), $db );
         $mw->map();
     }
-    
+
 }
