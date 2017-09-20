df4e1efa655ecbfeec045adb91bfbb3b96928cce
bartikowskiw <bartikowskiw@missouri.edu>
2017-09-20 13:26:44 -0500

    Unified naming to underscore.

--- src/MapReadCommand.php
+++ src/MapReadCommand.php
@@ -18,17 +18,23 @@ class MapReadCommand extends Command {
         $this
             ->setName( 'map:read' )
             ->setDescription( 'Reads a record from a lookup table created by map:write.' )
-            ->addArgument( 'record-id', InputArgument::REQUIRED, 'The record id. Probably the OCLC number.' )
-            ->addArgument( 'marc-file', InputArgument::REQUIRED, 'Path to MARC file' )
-            ->addArgument( 'sqlite-file', InputArgument::REQUIRED, 'Path to SQLite file' )
+            ->addArgument( 'record_id', InputArgument::REQUIRED, 'The record id. Probably the OCLC number.' )
+            ->addArgument( 'marc_file', InputArgument::REQUIRED, 'Path to MARC file' )
+            ->addArgument( 'sqlite_file', InputArgument::OPTIONAL, 'Path to SQLite file. Defaults to the MARC file name with added ".db" extension.' )
             ->addOption( 'raw', 'r', InputOption::VALUE_NONE, 'Raw MARC output?' );
     }
 
+    protected function initialize( InputInterface $input, OutputInterface $output ) {
+        if ( empty( $input->getArgument( 'sqlite_file' ) ) ) {
+            $input->setArgument( 'sqlite_file', $input->getArgument( 'marc_file'  ) . '.db' );
+        }
+    }
+
     protected function execute( InputInterface $input, OutputInterface $output ) {
-        $db = new \SQLite3( $input->getArgument( 'sqlite-file' ) );
-        $mr = new MarcMapReader( $input->getArgument( 'marc-file' ), $db );
+        $db = new \SQLite3( $input->getArgument( 'sqlite_file' ) );
+        $mr = new MarcMapReader( $input->getArgument( 'marc_file' ), $db );
         try {
-            $records = $mr->get( $input->getArgument( 'record-id' ) );
+            $records = $mr->get( $input->getArgument( 'record_id' ) );
             foreach ( $records as $record ) {
                 if ( $input->getOption( 'raw' ) ) {
                         $output->writeln( $record->toRaw() );
