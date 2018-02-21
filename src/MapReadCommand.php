3e321cb8a2134d61bee445589a0447a9806af4f3
bartikowskiw <bartikowskiw@missouri.edu>
2018-02-21 09:35:28 -0600

    Add regexp functionality for map:read.

--- src/MapReadCommand.php
+++ src/MapReadCommand.php
@@ -3,13 +3,14 @@
 declare( strict_types = 1 );
 namespace Umlts\Marcli;
 
+use Umlts\MarcToolset\MarcDump;
+use Umlts\MarcToolset\MarcMapReader;
+use Umlts\MarcToolset\MarcRecordNotFoundException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Umlts\MarcToolset\MarcMapReader;
-use Umlts\MarcToolset\MarcDump;
 
 class MapReadCommand extends Command {
 
@@ -21,7 +22,8 @@ class MapReadCommand extends Command {
             ->addArgument( 'record_id', InputArgument::REQUIRED, 'The record id. Probably the OCLC number.' )
             ->addArgument( 'marc_file', InputArgument::REQUIRED, 'Path to MARC file' )
             ->addArgument( 'sqlite_file', InputArgument::OPTIONAL, 'Path to SQLite file. Defaults to the MARC file name with added ".db" extension.' )
-            ->addOption( 'raw', 'r', InputOption::VALUE_NONE, 'Raw MARC output?' );
+            ->addOption( 'raw', 'r', InputOption::VALUE_NONE, 'Raw MARC output?' )
+            ->addOption( 'regexp', 'e', InputOption::VALUE_NONE, 'record_id is a regular expression' );
     }
 
     protected function initialize( InputInterface $input, OutputInterface $output ) {
@@ -34,15 +36,24 @@ class MapReadCommand extends Command {
         $db = new \SQLite3( $input->getArgument( 'sqlite_file' ) );
         $mr = new MarcMapReader( $input->getArgument( 'marc_file' ), $db );
         try {
-            $records = $mr->get( $input->getArgument( 'record_id' ) );
+            $records = $mr->get(
+                $input->getArgument( 'record_id'),
+                $input->getOption( 'regexp' )
+            );
+
             foreach ( $records as $record ) {
+                
                 if ( $input->getOption( 'raw' ) ) {
-                        $output->writeln( $record->toRaw() );
-                } else {
-                    $output->writeln(
-                        MarcDump::formatDump( (string) $record )
-                    );
+                    $output->writeln( $record->toRaw() );
+                    continue;
                 }
+                
+                if ( $input->getOption( 'no-ansi' ) ) {
+                    $output->writeln( (string) $record );
+                    continue;
+                }
+                
+                $output->writeln( MarcDump::formatDump( (string) $record ) );
             }
         } catch ( MarcRecordNotFoundException $e ) {
             $output->writeln( 'Record not found.' );
