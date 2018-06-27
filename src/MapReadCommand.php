e904300290437cc2002a7011b6d535d14fbc71a7
bartikowskiw <bartikowskiw@missouri.edu>
2018-06-27 10:09:26 -0500

    Fix bug (arg names).

--- src/MapReadCommand.php
+++ src/MapReadCommand.php
@@ -19,7 +19,7 @@ class MapReadCommand extends Command {
         $this
             ->setName( 'map:read' )
             ->setDescription( 'Reads a record from a lookup table created by map:write.' )
-            ->addArgument( 'record_id', InputArgument::REQUIRED, 'The record id. Probably the OCLC number.' )
+            ->addArgument( 'record-id', InputArgument::REQUIRED, 'The record id. Probably the OCLC number.' )
             ->addArgument( 'marc-file', InputArgument::REQUIRED, 'Path to MARC file' )
             ->addArgument( 'sqlite-file', InputArgument::OPTIONAL, 'Path to SQLite file. Defaults to the MARC file name with added ".db" extension.' )
             ->addOption( 'raw', 'r', InputOption::VALUE_NONE, 'Raw MARC output?' )
@@ -28,7 +28,7 @@ class MapReadCommand extends Command {
 
     protected function initialize( InputInterface $input, OutputInterface $output ) {
         if ( empty( $input->getArgument( 'sqlite-file' ) ) ) {
-            $input->setArgument( 'sqlite-file', $input->getArgument( 'marc_file'  ) . '.db' );
+            $input->setArgument( 'sqlite-file', $input->getArgument( 'marc-file'  ) . '.db' );
         }
     }
 
@@ -43,22 +43,22 @@ class MapReadCommand extends Command {
         $mr = new MarcMapReader( $input->getArgument( 'marc-file' ), $db );
         try {
             $records = $mr->get(
-                $input->getArgument( 'record_id'),
+                $input->getArgument( 'record-id'),
                 $input->getOption( 'regexp' )
             );
 
             foreach ( $records as $record ) {
-                
+
                 if ( $input->getOption( 'raw' ) ) {
                     $output->writeln( $record->toRaw() );
                     continue;
                 }
-                
+
                 if ( $input->getOption( 'no-ansi' ) ) {
                     $output->writeln( (string) $record );
                     continue;
                 }
-                
+
                 $output->writeln( MarcDump::formatDump( (string) $record ) );
             }
         } catch ( MarcRecordNotFoundException $e ) {
